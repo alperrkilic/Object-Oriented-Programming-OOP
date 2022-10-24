3e5677415627bf630c563c4c3430013c844d397e
Bayram Alper KILIÇ <balper78645@hotmail.com>
2022-10-24 23:34:39 +0300

    Cpp-Pointer

--- Pointers/pointer_artihmetic2.cpp
+++ Pointers/pointer_artihmetic2.cpp
@@ -46,10 +46,37 @@ int main(void){
 	
 	string *p1{&s1};
 	string *p2{&s2};
-	string *p3{&s3};
+	string *p3{&s1};
+	
+	cout<<boolalpha;
+	
+	cout<<"\nAddress: "<<endl;
+	cout<<p1<<" == "<<p2<<" : "<<(p1==p2)<<endl; //compares address
+	cout<<p2<<" == "<<p2<<" : "<<(p1==p3)<<endl; //compares address
+	
+	cout<<"\nValue: "<<endl;
+	cout<<*p1<<" == "<<*p2<<" : "<<(*p1 == *p2)<<endl;
+	cout<<*p1<<" == "<<*p3<<" : "<<(*p1 == *p3)<<endl;
 	
 	
-		
+	p3=&s3; // p3 now points james
+	
+	cout<<*p1<<" == "<<*p3<<" : "<<(*p1==*p3)<<endl;
+	
+	
+	cout<<"\n----------------"<<endl;
+	
+	char name[] {"Frank"};
+	
+	char *char_ptr1{nullptr};
+	char *char_ptr2{nullptr};
+	
+	char_ptr1 = &name[0];
+	char_ptr2 = &name[3];
+	
+	
+	cout<<"In the string "<<name<<", "<<*char_ptr2<<" is "<<(char_ptr2-char_ptr1)<<" characters away from "<<*char_ptr1<<endl;
+	
 	
 	
 	
